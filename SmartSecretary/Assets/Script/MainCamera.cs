83b023d674ea6caa64eeffd0b16152d2f8139db6
onestar <gkswndus10@naver.com>
2017-05-26 20:47:17 +0900

    Update 05.26

---
+++ SmartSecretary/Assets/Script/MainCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 25f;
+    public float offsetZ = -35f;
+    public float followSpeed = 10f;
+
+    Vector3 cameraPosition;
+
+    private void LateUpdate()
+    {
+//        cameraPosition.x = player.transform.position.x + offsetX;
+//        cameraPosition.y = player.transform.position.y + offsetY;
+//        cameraPosition.z = player.transform.position.z + offsetZ;
+//
+//        transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
